c4bb8c5d4331a16b140e93882e689e0c0aa7b97a
Tommy Yu <145308009+xinzhe-yu@users.noreply.github.com>
2026-09-06 14:56:45 -0700

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int L = 0;
+        int R = matrix.size() - 1;
+
+        
+        int row; 
+
+        while (L <= R) {
+            int midpoint = (L + R) / 2;
+            if (target < matrix[midpoint][0]) {
+                R = midpoint - 1;
+            } else if (target > matrix[midpoint].back()) {
+                L = midpoint + 1;
+            } else {
+                row = midpoint;
+                break;
+            }
+        }
+
+        if (row == -1) return false;
+
+        L = 0;
+        R = matrix[row].size() - 1;
+
+        while (L <= R) {
+            int m = (L + R) / 2;
+            if (target > matrix[row][m]) {
+                L = m + 1;
+            }
+            else if (target < matrix[row][m]) {
+                R = m - 1;
+            }
+            else if (target == matrix[row][m]) {
+                return true; 
+            }
+        }
+
+        return false; 
+
+
+    }
+};
