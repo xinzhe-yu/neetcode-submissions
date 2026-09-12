9cd1f272f757c1462703a45826e2e1ee1183c99c
Tommy Yu <145308009+xinzhe-yu@users.noreply.github.com>
2026-09-11 22:12:21 -0700

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    int dfs(TreeNode* root) {
+        if (root == nullptr) {
+            return 0; 
+        }
+
+        int left = dfs(root->left);
+
+        int right = dfs(root->right);
+
+        return max(left, right) + 1;
+
+
+    }
+public:
+    int maxDepth(TreeNode* root) {
+        return dfs(root);
+    }
+
+    
+};
