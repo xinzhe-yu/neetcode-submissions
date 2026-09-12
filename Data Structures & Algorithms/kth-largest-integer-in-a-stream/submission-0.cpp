748f2363600ce432c007519ec84164f980fcc11c
Tommy Yu <145308009+xinzhe-yu@users.noreply.github.com>
2026-09-11 21:27:06 -0700

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.cpp
@@ -0,0 +1,32 @@
+class KthLargest {
+private:
+    priority_queue<int> maxh;
+    int k;
+
+public:
+    KthLargest(int k, vector<int>& nums) {
+        for (auto& n : nums) {
+            maxh.push(n);
+        }
+        this->k = k;
+    }
+    
+    int add(int val) {
+        maxh.push(val);
+        vector<int> tmp;
+        for (int i = 0; i < k - 1; i++) {
+            tmp.push_back(maxh.top());
+            maxh.pop();
+        }
+        int max = maxh.top();
+
+        for (int i = 0; i < k - 1; i++) {
+            maxh.push(tmp.back());
+            tmp.pop_back();
+        }
+
+
+
+        return max;
+    }
+};
